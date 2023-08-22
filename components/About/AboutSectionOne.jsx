3be72a382537fb74fb654d7ec55917cd2a92a356
Joshua Waiswa <jwaiswa7@gmail.com>
2023-08-22 12:32:22 +0300

    Add cleaning services image

--- components/About/AboutSectionOne.jsx
+++ components/About/AboutSectionOne.jsx
@@ -49,7 +49,7 @@ const AboutSectionOne = () => {
               </div>
             </div>
 
-            <div className="w-full px-4 lg:w-1/2">
+            <div className="w-full px-4 lg:w-1/4">
               <div
                 className="wow fadeInUp relative mx-auto aspect-[25/24] max-w-[500px] lg:mr-0"
                 data-wow-delay=".2s"
@@ -58,6 +58,20 @@ const AboutSectionOne = () => {
                   src="/images/about/truck.png"
                   alt="about-image"
                   fill
+                  className="mx-auto max-w-full lg:mr-0"
+                />
+              </div>
+            </div>
+            <div className="w-full px-4 lg:w-1/4">
+              <div
+                className="wow fadeInUp relative mx-auto aspect-[25/24] max-w-[500px] lg:mr-0"
+                data-wow-delay=".2s"
+              >
+                <Image
+                  src="/images/about/cleaning_services.jpeg"
+                  alt="about-image"
+                  width={500}
+                  height={400}
                   className="mx-auto max-w-full lg:mr-0"
                 />
               </div>
