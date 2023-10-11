866baa52be109c0e74a6c5f170d908b4b4089137
Joshua Waiswa <jwaiswa7@gmail.com>
2023-10-11 14:29:40 +0300

    Update meta data

--- app/head.jsx
+++ app/head.jsx
@@ -3,7 +3,7 @@ export default function Head() {
     <>
       <title>macke company</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="To provide an ultimate service with human dignity." />
+      <meta name="description" content="Farming, tourism, hospitality, travel, logitics, Uganda, kenya, east Africa, Tanzania, Rwanda" />
       <meta name="vision" content="Macke believes in having a lasting impact on our clients, partners and communities within our operations. It is our desired vision to offer the best possible service and products while creating impact through" />
       <link rel="icon" href="/images/favicon.ico" />
     </>
